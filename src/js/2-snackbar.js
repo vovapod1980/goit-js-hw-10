d199af6a2b63b788c987d702ff5dd5e1d9a980d4
Volodymyr Podoliako <vpodol1980@gmail.com>
2026-03-10 15:16:01 +0200

    init commit

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -1,1 +1,39 @@
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
 
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', event => {
+  event.preventDefault();
+
+  const delay = Number(event.currentTarget.elements.delay.value);
+  const state = event.currentTarget.elements.state.value;
+
+  createPromise(delay, state)
+    .then(value => {
+      iziToast.success({
+        message: `✅ Fulfilled promise in ${value}ms`,
+        position: 'topRight',
+      });
+    })
+    .catch(value => {
+      iziToast.error({
+        message: `❌ Rejected promise in ${value}ms`,
+        position: 'topRight',
+      });
+    });
+
+  form.reset();
+});
+
+function createPromise(delay, state) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (state === 'fulfilled') {
+        resolve(delay);
+      } else {
+        reject(delay);
+      }
+    }, delay);
+  });
+}
